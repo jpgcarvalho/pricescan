86a22ffbb4d5a1e7f865bdead9b94f78a0577cf1
jpgcarvalho <peugomescarvalho2@gmail.com>
2024-11-22 18:16:58 -0300

    Initial commit

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,11 @@
+import styles from "@/app/Home/styles";
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Olá, Mundo</Text>
+    </View>
+  );
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,6 @@
+import Home from "@/app/Home";
+import React from "react";
+
+export default function Index() {
+  return <Home />;
+}
